0ea2e44ce43f2639098887f2f3ab8d496338de39
Danilo0203 <danilo.0203@gmail.com>
2025-05-04 00:42:21 -0600

    [UPDATE] Se mejoró el hook de nuevo cliente para permitir la edición de clientes existentes, se agregó la función de actualización y se ajustaron los valores predeterminados del formulario.

--- src/hooks/registro/nuevoClienteForm.hook.ts
+++ src/hooks/registro/nuevoClienteForm.hook.ts
@@ -1,4 +1,4 @@
-import { newClient } from '@/actions/registro/actions';
+import { newClient, updateClient } from '@/actions/registro/actions';
 import {
   NuevoClienteSchema,
   nuevoClienteType
@@ -7,25 +7,28 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { toast } from 'sonner';
 import { useMemo } from 'react';
-
+import { useRouter } from 'next/navigation';
+import { parseISO } from 'date-fns';
 export const useNuevoClienteForm = (onSuccess?: () => void, cliente?: any) => {
   const date = useMemo(() => new Date(), []);
+  const isEdit = Boolean(cliente);
+  const router = useRouter();
   const form = useForm<nuevoClienteType>({
     resolver: zodResolver(NuevoClienteSchema),
     defaultValues: {
       nombre: cliente?.nombre ?? '',
-      telefono: cliente?.phone ?? undefined,
+      telefono: Number(cliente?.phone) ?? undefined,
       fechaNacimiento: cliente?.fechaNacimiento
-        ? new Date(cliente.fechaNacimiento)
+        ? parseISO(cliente.fechaNacimiento)
         : undefined,
       sexo: cliente?.sexo ?? undefined,
       estatura: cliente?.estatura ?? undefined,
       peso: cliente?.peso ?? undefined,
       tipoCuerpo: cliente?.tipoCuerpo ?? '',
-      inscripcion: date,
+      inscripcion: cliente?.inscripcion ? parseISO(cliente.inscripcion) : date,
       plan: cliente?.plan ?? undefined,
       valor: cliente?.valor ?? undefined,
-      fechaInicio: date,
+      fechaInicio: cliente?.fechaInicio ? parseISO(cliente.fechaInicio) : date,
       descuento: cliente?.descuento ?? undefined,
       total: cliente?.total ?? undefined,
       diasPorSemana: cliente?.diasPorSemana ?? undefined,
@@ -35,26 +38,43 @@ export const useNuevoClienteForm = (onSuccess?: () => void, cliente?: any) => {
   });
 
   const onSumbit = async (data: nuevoClienteType) => {
-    const error = await newClient(data);
-    if (error) {
-      if (
-        error.message.includes(
-          'Phone number already registered by another user'
-        )
-      ) {
-        toast.error(
-          'El número de teléfono ya está registrado por otro usuario',
-          {
-            position: 'top-right'
-          }
-        );
+    if (isEdit) {
+      const response = await updateClient({
+        ...data,
+        userID: cliente.userID
+      });
+      if (response.code !== 'P0001') {
+        toast.success('Cliente actualizado correctamente', {
+          position: 'top-right'
+        });
+        router.push('/panel/clientes');
+      } else {
+        toast.error(response.message, {
+          position: 'top-right'
+        });
       }
     } else {
-      toast.success('Cliente creado correctamente', {
-        position: 'top-right'
-      });
-      form.reset();
-      if (onSuccess) onSuccess();
+      const error = await newClient(data);
+      if (error) {
+        if (
+          error.message.includes(
+            'Phone number already registered by another user'
+          )
+        ) {
+          toast.error(
+            'El número de teléfono ya está registrado por otro usuario',
+            {
+              position: 'top-right'
+            }
+          );
+        }
+      } else {
+        toast.success('Cliente creado correctamente', {
+          position: 'top-right'
+        });
+        form.reset();
+        if (onSuccess) onSuccess();
+      }
     }
   };
   return { form, onSumbit, reset: form.reset };
